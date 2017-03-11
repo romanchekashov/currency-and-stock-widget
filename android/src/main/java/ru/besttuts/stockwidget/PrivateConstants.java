3da2364a20e88283ac7704ec5d19caf975840e7b
romanchekashov <romanrich89@gmail.com>
2017-03-11 16:07:15 +0300

    cherry-pick this commit before gen signed apk

--- android/src/main/java/ru/besttuts/stockwidget/PrivateConstants.java
+++ android/src/main/java/ru/besttuts/stockwidget/PrivateConstants.java
@@ -5,7 +5,16 @@ package ru.besttuts.stockwidget;
  */
 
 public class PrivateConstants {
-    public static final String BANNER_AD_UNIT_ID = "";
-    public static final String INTERSTITIAL_AD_UNIT_ID = "";
-    public static final String HASHED_DEVICE_ID = "";
+
+    // Google Ads
+    public static final String BANNER_AD_UNIT_ID = "ca-app-pub-6594301847430371/1641284047";
+    public static final String INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-6594301847430371/3118017248";
+    public static final String HASHED_DEVICE_ID = "D3C980644AEC6606384943F6D91";
+
+    // GMS
+    public static final String SENDER_ID = "759752250393";
+
+    // Google Analytics
+    public static final String GA_TRACKING_ID = "UA-60087009-1";
+
 }
