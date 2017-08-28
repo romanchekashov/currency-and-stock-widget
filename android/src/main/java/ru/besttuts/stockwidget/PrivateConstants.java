866795798d91f65a76f7ca85d06bb3ad227f0683
romanchekashov <romanrich89@gmail.com>
2017-08-28 05:05:09 +0300

    set dev mode

--- android/src/main/java/ru/besttuts/stockwidget/PrivateConstants.java
+++ android/src/main/java/ru/besttuts/stockwidget/PrivateConstants.java
@@ -7,14 +7,14 @@ package ru.besttuts.stockwidget;
 public class PrivateConstants {
 
     // Google Ads
-    public static final String BANNER_AD_UNIT_ID = "ca-app-pub-6594301847430371/1641284047";
-    public static final String INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-6594301847430371/3118017248";
-    public static final String HASHED_DEVICE_ID = "D3C980644AEC6606384943F6D91";
+    public static final String BANNER_AD_UNIT_ID = "";
+    public static final String INTERSTITIAL_AD_UNIT_ID = "";
+    public static final String HASHED_DEVICE_ID = "";
 
     // GMS
-    public static final String SENDER_ID = "759752250393";
+    public static final String SENDER_ID = "";
 
     // Google Analytics
-    public static final String GA_TRACKING_ID = "UA-60087009-1";
+    public static final String GA_TRACKING_ID = "";
 
 }
