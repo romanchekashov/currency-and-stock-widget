352ab75abba64f022915d594d9519a6ed8143b73
romanchekashov <romanrich89@gmail.com>
2017-11-30 04:05:23 +0300

    update v.2.1.5

--- android/src/main/java/ru/besttuts/stockwidget/service/UpdateService.java
+++ android/src/main/java/ru/besttuts/stockwidget/service/UpdateService.java
@@ -5,8 +5,29 @@ import android.appwidget.AppWidgetManager;
 import android.content.Intent;
 import android.os.IBinder;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import ru.besttuts.stockwidget.model.Model;
+import ru.besttuts.stockwidget.model.Setting;
+import ru.besttuts.stockwidget.provider.db.DbProvider;
 import ru.besttuts.stockwidget.service.tasks.FetchStockDataAsyncTask;
+import ru.besttuts.stockwidget.sync.deserializer.YahooMultiQueryDataDeserializer;
+import ru.besttuts.stockwidget.sync.model.YahooMultiQueryData;
 import ru.besttuts.stockwidget.ui.EconomicWidget;
+import ru.besttuts.stockwidget.util.CustomConverter;
+import ru.besttuts.stockwidget.util.YahooQueryBuilder;
 
 import static ru.besttuts.stockwidget.util.LogUtils.LOGD;
 import static ru.besttuts.stockwidget.util.LogUtils.LOGE;
@@ -33,7 +54,9 @@ public class UpdateService extends Service {
         final int[] allWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
         boolean hasInternet = intent.getBooleanExtra(EconomicWidget.ARG_HAS_INTERNET, true);
 
-        new FetchStockDataAsyncTask(this, appWidgetManager, allWidgetIds, startId, hasInternet).execute();
+//        new FetchStockDataAsyncTask(this, appWidgetManager, allWidgetIds, startId, hasInternet).execute();
+
+        updateData(this, appWidgetManager, allWidgetIds, startId, hasInternet);
 
         return super.onStartCommand(intent, flags, startId);
     }
@@ -50,4 +73,73 @@ public class UpdateService extends Service {
         LOGD(TAG, "onDestroy: Current thread: " + Thread.currentThread().getName());
     }
 
+    private void updateData(final Service service, final AppWidgetManager appWidgetManager,
+                            final int[] allWidgetIds, final int startId, final boolean hasInternet){
+        final List<Setting> settings = DbProvider.getInstance().getAllSettingsWithCheck();
+        String url = YahooQueryBuilder.buildYahooFinanceMultiQueryUrl(settings);
+        final Gson gson = new GsonBuilder().registerTypeAdapter(YahooMultiQueryData.class,
+                new YahooMultiQueryDataDeserializer()).create();
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        LOGD(TAG, "Response is: "+ response.substring(0,500));
+                        YahooMultiQueryData yahooMultiQueryData = gson.fromJson(response, YahooMultiQueryData.class);
+                        Map<Integer, List<Model>> map = map(settings, yahooMultiQueryData);
+                        updateWidget(map, service, appWidgetManager, allWidgetIds, startId, hasInternet);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                LOGE(TAG, "That didn't work!");
+            }
+        });
+        requestQueue.add(stringRequest);
+    }
+
+    private Map<Integer, List<Model>> map(List<Setting> settings, YahooMultiQueryData yahooMultiQueryData){
+        Map<Integer, List<Model>> map = new HashMap<>();
+        DbProvider dbProvider = DbProvider.getInstance();
+        Map<String, Model> symbolModelMap = CustomConverter.convertToModelMap(yahooMultiQueryData);
+
+        for (Setting setting : settings) {
+            int widgetId = setting.getWidgetId();
+            if (!map.containsKey(widgetId)) {
+                map.put(widgetId, new ArrayList<Model>());
+            }
+            map.get(widgetId).add(symbolModelMap.get(setting.getQuoteSymbol()));
+        }
+
+        for (Map.Entry<Integer, List<Model>> me : map.entrySet()) {
+            List<Model> models = me.getValue();
+
+            for (int i = 0, l = models.size(); i < l; i++) {
+                Model model = models.get(i);
+                if (null == model) continue;
+                if(!dbProvider.addModelRec(model)){
+                    models.set(i, dbProvider.getModelById(model.getId()));
+                }
+            }
+        }
+
+        // при успешном получении данных, удаляем статус о проблемах соединения
+        EconomicWidget.connectionStatus = null;
+
+        return map;
+    }
+
+    private void updateWidget(Map<Integer, List<Model>> map, Service service, AppWidgetManager appWidgetManager,
+                              int[] allWidgetIds, int startId, boolean hasInternet){
+
+        for (int widgetId: allWidgetIds) {
+            EconomicWidget.updateAppWidget(service.getApplicationContext(), appWidgetManager, widgetId,
+                    map.get(widgetId), hasInternet);
+        }
+
+        boolean serviceStopped = (null != service) && service.stopSelfResult(startId);
+        LOGD(TAG, "Load Yahoo Finance Thread#" + startId + " end, can be stopped: " + serviceStopped);
+        LOGD(TAG, "onPostExecute: Current thread: " + Thread.currentThread().getName());
+    }
 }
--- android/src/main/java/ru/besttuts/stockwidget/util/YahooQueryBuilder.java
+++ android/src/main/java/ru/besttuts/stockwidget/util/YahooQueryBuilder.java
@@ -1,6 +1,11 @@
 package ru.besttuts.stockwidget.util;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import ru.besttuts.stockwidget.model.QuoteType;
+import ru.besttuts.stockwidget.model.Setting;
 
 /**
  * @author rchekashov
@@ -13,6 +18,30 @@ public class YahooQueryBuilder {
 
     private static String yahooFinanceXchangeQuery = "select * from yahoo.finance.xchange where pair in (";
     private static String yahooFinanceQuotesQuery = "select * from yahoo.finance.quotes where symbol in (";
+
+    public static String buildYahooFinanceMultiQueryUrl(List<Setting> settings){
+        String encodedYahooQuery = Utils.encodeYahooApiQuery(buildYahooFinanceMultiQuery(settings));
+        return HTTP_QUERY_YAHOOAPIS_COM_V1_PUBLIC + "yql?" + encodedYahooQuery;
+    }
+
+    public static String buildYahooFinanceMultiQuery(List<Setting> settings){
+        Set<String> currencyExchangeSet = new HashSet<>();
+        Set<String> goodSet = new HashSet<>();
+        for (Setting setting : settings) {
+            switch (setting.getQuoteType()) {
+                case QuoteType.CURRENCY:
+                    currencyExchangeSet.add(setting.getQuoteSymbol());
+                    break;
+                case QuoteType.GOODS:
+                case QuoteType.INDICES:
+                case QuoteType.STOCK:
+                case QuoteType.QUOTES:
+                    goodSet.add(setting.getQuoteSymbol());
+                    break;
+            }
+        }
+        return buildYahooFinanceMultiQuery(currencyExchangeSet, goodSet);
+    }
 
     public static String buildYahooFinanceMultiQuery(Set<String> currencyExchangeSet, Set<String> goodSet) {
         StringBuilder builder = new StringBuilder();
